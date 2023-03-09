16334bdd1bce6ba269f2090d6c1e9b7fc65c693c
lnntrinh0112 <avs73ltqb@gmail.com>
2023-03-09 12:42:19 +0700

    Associate a URL path with a component

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:productId', component: ProductDetailsComponent },
+  { path: '', component: ProductDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     ProductListComponent,
     TopBarComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
